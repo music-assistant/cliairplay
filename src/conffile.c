4a9120cb292b650814ca5bca724c02ba399b95f4
Brad Keifer <bradkeifer@icloud.com>
2026-03-08 18:53:37 +1100

    Add commentary about purpose of timer_test config value. Can be helpful for ffmpeg debugging

--- src/conffile.c
+++ src/conffile.c
@@ -66,7 +66,7 @@ static cfg_opt_t sec_general[] =
     CFG_STR("allow_origin", "*", CFGF_NONE),
     CFG_STR("user_agent", PACKAGE_NAME "/" PACKAGE_VERSION, CFGF_NONE),
     CFG_BOOL("ssl_verifypeer", cfg_true, CFGF_NONE),
-    CFG_BOOL("timer_test", cfg_false, CFGF_NONE),
+    CFG_BOOL("timer_false", cfg_true, CFGF_NONE), // if true, then ignores input and plays ticks
     CFG_INT("start_buffer_ms", 2250, CFGF_NONE),
     CFG_END()
   };
